1289913bd4fe8c48c3088228813a431fb1d6dc5a
awzhenkang <148183055+awzhenkang@users.noreply.github.com>
2024-04-16 20:41:34 +0800

    Q2

--- Question2.java
+++ Question2.java
@@ -10,9 +10,6 @@ public class Question2 {
 
         double BMI = weight / (height * height);
         
-        // Format BMI to 1 decimal place using String.format()
-        String formattedBMI = String.format("%.1f", BMI);
-        
-        System.out.println("Your BMI is " + formattedBMI);
+        System.out.println("Your BMI is " + BMI);
     }
 }
